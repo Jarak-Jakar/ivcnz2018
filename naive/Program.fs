746b94e20d0c66f3ced40fe3262693f7a8b61fe8
James Cooper <jcoo092@UoA.auckland.ac.nz>
2018-09-05 11:04:18 +1200

    Attempted to change approach
    Modified program to adapt to using a byte array, but conversion back to a final image doesn't seem to be working...
    
    Will try techniques found at https://stackoverflow.com/questions/50025908/convert-imagergba32-to-byte-using-imagesharp

--- naive/Program.fs
+++ naive/Program.fs
@@ -1,27 +1,32 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
 open SixLabors.ImageSharp
 open SixLabors.ImageSharp.Advanced
 open SixLabors.ImageSharp.PixelFormats
+open SixLabors.ImageSharp.Formats.Jpeg
 
-let zeroClamp width height x y =
+(* let zeroClamp width height x y =
     let w = width - 1
     let h = height - 1
     if x < 0 || x > w || y < 0 || x > w then
         (0,0)
     else
-        (x,y)
+        (x,y) *)
 
-let accessClampedArrayWithDefault arr def x y =
-    let w = (Array.length arr) - 1
-    let h = (Array.length arr.[0]) - 1
-    if x < 0 || x > w || y < 0 || x > w then
+let accessClampedArrayWithDefault (arr: uint32[][]) width height def x y : uint32[] =
+    if x < 0 || x > width-1 || y < 0 || y > height-1 then
         def
     else
-        arr.[x].[y]
+        arr.[x + width * y]
 
-let inline (+^) (a: PixelFormats.Rgba32) (b: PixelFormats.Rgba32): PixelFormats.Rgba32 =
+let extractPixelParts (p: Rgba32) =
+    let R = uint32 p.R
+    let G = uint32 p.G
+    let B = uint32 p.B
+    let A = uint32 p.A
+    [|R; G; B; A|]
+
+(* let inline (+^) (a: PixelFormats.Rgba32) (b: PixelFormats.Rgba32): PixelFormats.Rgba32 =
     let A = a.A + b.A
     let R = a.R + b.R
     let G = a.G + b.G
@@ -33,23 +38,39 @@ let inline (/^) (px: PixelFormats.Rgba32) (divisor: float) =
     let R = float px.R / divisor |> byte
     let G = float px.G / divisor |> byte
     let B = float px.B / divisor |> byte
-    Rgba32(R, G, B, A)
+    Rgba32(R, G, B, A) *)
 
 [<EntryPoint>]
 let main argv =
     //printfn "Hello World from F#!"
     use img = Image.Load(@"D:\Users\jcoo092\Writing\2018\IVCNZ18\cute-puppy.jpg")
 
-    //printfn "img width is %d, height is %d" img.Width img.Height
-
     let mutable out_img = img.Clone()
 
-    let pxs = Array.init img.Height (fun x -> img.GetPixelRowSpan(x).ToArray())
+    let pxs = img.GetPixelSpan().ToArray() |> Array.map extractPixelParts
 
-    //printfn "pxs.Length is %d, pxs.[0].Length is %d" pxs.Length pxs.[0].Length
+    let mutable (nps: uint32[][]) = Array.zeroCreate pxs.Length
 
-    let mutable (nps: PixelFormats.Rgba32[][]) = Array.copy pxs
-    let zc = accessClampedArrayWithDefault pxs (Rgba32(0uy, 0uy, 0uy, 0uy))
+    let ac = accessClampedArrayWithDefault pxs img.Width img.Height [|0u;0u;0u;0u|]
+
+    for x in 0..img.Width-1 do
+        for y in 0..img.Height-1 do
+            let p = ac x y
+            for z in -1..1 do
+                for w in -1..1 do
+                    let q = ac (x + z) (y + w)
+                    nps.[x + y * img.Width] <- Array.zip p q |> Array.map (fun (a,b) -> a + b)
+            nps.[x + y * img.Width] <- Array.map (fun i -> float i / 9.0 |> uint32 ) nps.[x + y * img.Width]
+
+    let abc = Array.collect (fun a -> Array.map byte a) nps
+
+    //printfn "Detected format is %A" (img.GetConfiguration())
+
+    let potato = Image.Load<Rgba32>(img.GetConfiguration(), abc, Formats.Jpeg.JpegDecoder())
+
+    printfn "potato's width is %d and height is %d" potato.Width potato.Height
+
+    (* let zc = accessClampedArrayWithDefault pxs (Rgba32(0uy, 0uy, 0uy, 0uy))
 
     for x in 0..img.Height-1 do
         for y in 0..img.Width-1 do
@@ -59,9 +80,9 @@ let main argv =
                     let q = zc (y + z) (x + w)
                     nps.[x].[y] <- nps.[x].[y] +^ q
             nps.[x].[y] <- nps.[x].[y] /^ 9.0
-            out_img.[y, x] <- nps.[x].[y]
+            out_img.[y, x] <- nps.[x].[y] *)
 
-    out_img.Save(@"D:\Users\jcoo092\Writing\2018\IVCNZ18\naive_output.jpg")
+    //out_img.Save(@"D:\Users\jcoo092\Writing\2018\IVCNZ18\naive_output.jpg")
 
 
 
