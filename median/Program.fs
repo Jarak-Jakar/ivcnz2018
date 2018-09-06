8354e76f51b41a6f0b5721764cd6f32c47dba33c
Jarak <jarak_jakar@hotmail.com>
2018-09-06 20:37:09 +1200

    Started on the median
    Created a new project for the semi-optimised estimation of the window median.

---
+++ median/Program.fs
@@ -0,0 +1,17 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open SixLabors.ImageSharp
+open SixLabors.ImageSharp.Processing
+open SixLabors.ImageSharp.Advanced
+
+[<EntryPoint>]
+let main argv =
+
+    let img = Image.Load(@"..\..\cute-puppy.jpg")
+    img.Mutate(fun x -> x.Grayscale() |> ignore)
+
+    printfn "%A" ((Array.skip 100 (img.GetPixelSpan().ToArray()) |> Array.take 20) |> Array.map (fun p -> p.R))
+
+    //printfn "Hello World from F#!"
+    0 // return an integer exit code
