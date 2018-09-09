68e23d6da151580aead5a2f1d8c071e8a885f24c
Jarak <jarak_jakar@hotmail.com>
2018-09-09 17:34:42 +1200

    Kept working on CML
    Didn't get very far at all though...

--- cml/Program.fs
+++ cml/Program.fs
@@ -4,27 +4,58 @@ open System
 open SixLabors.ImageSharp
 open SixLabors.ImageSharp.Processing
 open Hopac
+open Hopac.Core
+
+(* type Pix<'a> = {
+    pCh: Ch<'a>
+} *)
 
 type Pix<'a> = {
-    pCh: Ch<'a>
-    sendCount: int
-    recvCount: int
+    c: Ch<'a>
+    x: int
+    y: int
+    i: 'a
 }
 
-let stillToComm p =
-    if p.sendCount > 0 || p.recvCount > 0 then
-        true
-    else
-        false
+let computeCoords width index =
+    (
+        index % width,
+        index / width
+    )
 
-let pix v c = job {
-    let p = {pCh = Ch ()}
-    let rec work v c = job {
-        let! m = Alt.choose [
-            Ch.take p.pCh ^=> printfn "Received %A"
-        ]
-    }
-}
+type Direction =
+    | West
+    | East
+    | North
+    | South
+    | Northwest
+    | Northeast
+    | Southwest
+    | Southeast
+
+let directions = [|West; East; North; South; Northwest; Northeast; Southwest; Southeast|]
+
+let getDisplacement x y = function
+    | West -> (x - 1, y)
+    | East -> (x + 1, y)
+    | North -> (x, y - 1)
+    | South -> (x, y + 1)
+    | Northwest -> (x - 1, y - 1)
+    | Northeast -> (x + 1, y - 1)
+    | Southwest -> (x - 1, y + 1)
+    | Southeast -> (x + 1, y + 1)
+
+(* let pix width height idx intensity = job {
+    let p = Ch ()
+    let (x, y) = computeCoords width idx
+    let neighbourIndices = Array.map (fun d -> getDisplacement x y d) directions
+
+    return p
+} *)
+
+let createPixel width index intensity =
+    let x, y = computeCoords width index
+    {c = Ch (); x = x; y = y; i = intensity}
 
 [<EntryPoint>]
 let main argv =
@@ -34,6 +65,9 @@ let main argv =
     img.Mutate(fun x -> x.Grayscale() |> ignore)
 
     img.Save(@"D:\Users\jcoo092\Writing\2018\IVCNZ18\sample_output.jpg") *)
+
+    let makePixels = createPixel 120
+    let intensities = Array.init (120 * 116) byte |> Array.mapi makePixels
 
 
 
