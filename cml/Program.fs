562909e39e59496c392793cd7e673264df91b5f9
Jarak <jarak_jakar@hotmail.com>
2018-09-13 18:59:03 +1200

    Continued working on the CML stuff.
    Not going great so far, but  I think I'm beginning to get somewhere.

--- cml/Program.fs
+++ cml/Program.fs
@@ -5,26 +5,21 @@ open SixLabors.ImageSharp
 open SixLabors.ImageSharp.Processing
 open Hopac
 open Hopac.Core
+open Hopac.Extensions
+open SixLabors.ImageSharp.PixelFormats
+open Hopac
 
 (* type Pix<'a> = {
     pCh: Ch<'a>
 } *)
 
 type Pix<'a> = {
-    c: Ch<'a>
+    c: Ch<Option<'a>>
     x: int
     y: int
-    i: 'a
+    i: Option<'a>
+    n: IVar<Option<'a>> []
 }
-
-let computeCoords width index =
-    (
-        index % width,
-        index / width
-    )
-
-let computeIndex width x y =
-    x + y * width
 
 type Direction =
     | West
@@ -37,6 +32,15 @@ type Direction =
     | Southeast
 
 let directions = [|West; East; North; South; Northwest; Northeast; Southwest; Southeast|]
+
+let computeCoords width index =
+    (
+        index % width,
+        index / width
+    )
+
+let computeIndex width x y =
+    x + y * width
 
 let getDisplacement x y = function
     | West -> (x - 1, y)
@@ -56,13 +60,16 @@ let getDisplacement x y = function
     return p
 } *)
 
-let createPixel width index intensity =
+let createPixel width windowSize index intensity =
     let x, y = computeCoords width index
-    {c = Ch (); x = x; y = y; i = intensity}
+    let p = {c = Ch (); x = x; y = y; i = Some(intensity); n = Array.create (windowSize * windowSize - 1) (IVar ());}
+    //printfn "%A" p.n.[0]
+    //IVar.fill p.n.[0] intensity |> run
+    p
 
 //val getNeighbourPix: Pix<'a> -> Direction -> Job<'a>
 
-let getNeighbourPixelIntensity width (pixelsArray: Pix<'a> []) pixel direction = 
+(* let getNeighbourPixelIntensity width (pixelsArray: Pix<'a> []) pixel direction =
     let displaceX, displaceY = getDisplacement pixel.x pixel.y direction
     let neighbourIdx = computeIndex width displaceX displaceY
     job {
@@ -70,12 +77,71 @@ let getNeighbourPixelIntensity width (pixelsArray: Pix<'a> []) pixel direction =
         return! intensity
     }
 
+let getNeighbourPixelIntensities width pixelsArray pixel =
+    Array.map (getNeighbourPixelIntensity width pixelsArray pixel) directions *)
 
-let runPixel pixel = 
-    run getNeighbourPixelIntensity width pixelsArray pixel direction
+let giveIntensity pix =
+    printfn "pix %d %d giving intensity %A" pix.x pix.y pix.i
+    Ch.give pix.c pix.i
+
+let lookupNeighbour (pixels: Pix<'a>[]) neighbourIdx =
+    if neighbourIdx < 0 || neighbourIdx >= pixels.Length then
+        Alt.always None
+    else
+        pixels.[neighbourIdx].c
+
+let getIntensity pix neighbourChan neighbourNum =
+    printfn "pix %d %d taking an intensity from neighbourNum %d" pix.x pix.y neighbourNum
+
+    Ch.take neighbourChan |> Alt.afterJob (fun x -> IVar.fill pix.n.[neighbourNum] x)
+
+let getAllIntensities width (pixels: Pix<'a> []) pix =
+    Array.mapi (fun i d ->
+                    let displaceX, displaceY = getDisplacement pix.x pix.y d
+                    let neighbourIdx = computeIndex width displaceX displaceY
+                    printfn "i = %d, neighbourIdx = %d" i neighbourIdx
+                    getIntensity pix pixels.[neighbourIdx] i)
+                directions
+
+let sendMessages width pixels pix =
+    let choices = Array.append (getAllIntensities width pixels pix) [|giveIntensity pix|]
+    Job.server << Job.iterate () <| fun () ->
+                                    Alt.choose choices
+    |> start
+
+let pullOutWindow pix = job {
+    Array.iteri (fun i v -> printfn "IVar %d is full? %A" i (IVar.Now.isFull v)) pix.n
+    //Array.iteri (fun i v -> printfn "IVar %d contains %A" i (IVar.Now.get v)) pix.n
+    return Array.map (fun x -> IVar.read x |> Alt.toAsync) pix.n |> Async.Parallel |> Async.RunSynchronously
+    //Array.map (fun x -> IVar.read x |> run) pix.n
+    //return! Array.map (fun x -> MVar.read x) pix.n
+}
+
+let arrayMedian arr = job {
+    let arr2 = Array.choose id arr
+    Array.Sort arr2
+    return arr2.[arr2.Length / 2]
+}
+
+(* let arrayMedian arr =
+    Array.Sort arr
+    arr.[arr.Length / 2] *)
+
+let findMedians pixArray =
+    Array.map (fun x -> pullOutWindow x |> Job.bind arrayMedian |> run) pixArray
+
+//let fms (fmarr: Job<'a>[]) = Array.Parallel.map run fmarr
+
+let makeRgba32 i =
+    Rgba32(i, i, i, 255uy)
+
+//let runPixel pixel =
+  //  run getNeighbourPixelIntensity width pixelsArray pixel direction
 
 [<EntryPoint>]
 let main argv =
+
+    //printfn "Is serverGC? %A" System.Runtime.GCSettings.IsServerGC
 
     (* use img = Image.Load(@"D:\Users\jcoo092\Writing\2018\IVCNZ18\cute-puppy.jpg")
 
@@ -83,11 +149,48 @@ let main argv =
 
     img.Save(@"D:\Users\jcoo092\Writing\2018\IVCNZ18\sample_output.jpg") *)
 
-    let makePixels = createPixel 120
-    let intensities = Array.init (120 * 116) byte |> Array.mapi makePixels
-    
+    let makePixels = createPixel 5 3 //img.Width
+    let intensities = Array.init (5 * 5) byte |> Array.mapi makePixels // temporary only, to give me pixel representations
+
+    let sm = sendMessages 5 intensities
+    Array.map sm intensities |> ignore
+
+    let res = findMedians intensities |> Array.Parallel.map makeRgba32 // this part pulls out the results
+
+    printfn "%A" res
+
+    (* let a = intensities.[0].n.[0]
+    let b = MVar.fill a 5uy
+    let c = Alt.always 5uy
+
+
+    let d = Ch.give intensities.[0].c intensities.[0].i
+    let e = Ch.take intensities.[1].c |> Alt.afterJob (fun x -> MVar.fill intensities.[0].n.[0] x) //|> Alt.afterFun (fun _ -> Alt.zero ())
+    let g = Alt.once e
+
+    let p = job {
+        return! Ch.take intensities.[1].c |> Alt.afterJob (fun x -> MVar.fill intensities.[0].n.[0] x)
+    }
+
+    let q = Alt.once p
+
+    let i = intensities.[0].n
+    //let j = Array.map (fun x -> Alt.toAsync x) i |> Async.Parallel |> Async.RunSynchronously
+    let k = Array.map (fun x -> MVar.read x |> Alt.toAsync) i |> Async.Parallel |> Async.RunSynchronously
+
+
+    Alt.choose [d; e; g] |> ignore *)
+
+  (*   let disps = Seq.map (getDisplacement 0 0) directions
+                |> Seq.collect (fun (x,y) -> Seq.map (getDisplacement x y) directions)
+                |> Seq.distinct
+                |> Seq.collect (fun (x,y) -> Seq.map (getDisplacement x y) directions)
+                |> Seq.distinct
+
+    printfn "%A %d" disps (Seq.length disps) *)
 
 
 
-    printfn "Hello World from F#!"
+
+    //printfn "Hello World from F#!"
     0 // return an integer exit code
