2f4c69435927cdd7f871f658ecf464986f1d5859
Jarak <jarak_jakar@hotmail.com>
2019-01-15 17:41:54 +1300

    Played around with a few different possibilites for improving the speed of the processWindow function.  Some turned out to be worse than the original implementation, but one proved to be approximately twice as fast.
    I think this version is actually now faster than my naive go version...
    
    If I can work out how to use ValueOptions, I will be curious to see how that will impact things.
    
    Change that would be good to make still is to introduce a pool of pre-allocated arrays.

--- naive/Program.fs
+++ naive/Program.fs
@@ -10,6 +10,7 @@ open SixLabors.ImageSharp.Formats.Png
 open System.IO
 open Microsoft.FSharp.Core.OptimizedClosures
 open System.Threading
+open System
 
 let timer = System.Diagnostics.Stopwatch()
 
@@ -23,47 +24,66 @@ let findMedian (l: 'a[]) =
     Array.sortInPlace l
     l.[(Array.length l)>>>1]
 
-let processWindow (clampedArrayFunc: FSharpFunc<_, _, _>) windowSize x y =
+(* let processWindow (clampedArrayFunc: FSharpFunc<_, _, _>) windowSize posBound x y =
     let meds = Array.zeroCreate (windowSize * windowSize)
-    let posBound = (windowSize - 1) / 2
     let negBound = -posBound
-    for z in negBound..posBound do
-        for w in negBound..posBound do
+    for w in negBound..posBound do
+        for z in negBound..posBound do
             meds.[(z + posBound) * windowSize + (w + posBound)] <- clampedArrayFunc.Invoke((x + z), (y + w))
-    Array.choose id meds |> findMedian
+    Array.choose id meds |> findMedian *)
 
-// let processWindow (intensities: 'a[]) width height offset x y =
+let processWindow (intensities: 'a[]) width height offset x y =
 
-//     let lhb = max 0 (x - offset)
-//     let uhb = min (width - 1) (x + offset)
-//     let lvb = max 0 (y - offset)
-//     let uvb = min (height - 1) (y + offset)
+    let lhb = max 0 (x - offset)
+    let uhb = min (width - 1) (x + offset)
+    let lvb = max 0 (y - offset)
+    let uvb = min (height - 1) (y + offset)
 
-//     (* for w in lvb..uvb do
-//         for z in lhb..uhb do
-//             meds.[z + windowSize * w] <- intensities.[(x + z) + width * (y + w)] *)
-//     (* let meds = [|for w in lvb..(uvb - 1) do
-//                     for z in lhb..(uhb - 1) do
-//                         yield intensities.[(x + z) + width * (y + w)]
-//         |] *)
-//     let mutable ystride = 0
-//     let meds = [|for w in lvb..uvb do
-//                         ystride <- w * width
-//                         yield intensities.[lhb + ystride .. uhb + ystride]
-//                 |] |> Array.concat
-//     findMedian meds
+    let meds = Array.zeroCreate ((uhb - lhb + 1) * (uvb - lvb + 1))
+
+    let mutable idx = 0
+
+    for w in lvb..uvb do
+        for z in lhb..uhb do
+            meds.[idx] <- intensities.[z + width * w]
+            idx <- idx + 1
+
+    (* let mutable meds' = List.Empty
+    for w in lvb..uvb do
+        for z in lhb..uhb do
+            meds' <- intensities.[z + width * w] :: meds'
+
+    let meds = List.toArray meds' *)
+
+
+    (* let meds = [|
+        for w in lvb..uvb do
+            for z in lhb..uhb do
+                yield intensities.[z + width * w]
+                |] *)
+
+    (* let mutable ystride = 0
+    let meds = [|for w in lvb..uvb do
+                        ystride <- w * width
+                        yield intensities.[lhb + ystride .. uhb + ystride]
+                |] |> Array.concat *)
+
+    findMedian meds
 
 let makeRgb24 r = Rgb24(r, r, r)
 
 let medianFilter (intensities: byte[]) width height windowSize =
     //printfn "offset is %d" offset
 
-    let ac = accessClampedArray intensities width height |> FSharpFunc<_,_,_>.Adapt
-    let pw = processWindow ac windowSize |> FSharpFunc<_, _, _>.Adapt
-    (* let offset = (windowSize - 1) >>> 1 // divide by 2
-    let pw = processWindow intensities width height offset |> FSharpFunc<_, _, _>.Adapt *)
+    let offset = (windowSize - 1) >>> 1 // divide by 2
 
-    let outputPixels = Array.Parallel.map (fun i -> // These calculations are fixed for the whole array.  Could maybe do some vectorisation of them?
+    (* let ac = accessClampedArray intensities width height |> FSharpFunc<_,_,_>.Adapt
+    let pw = processWindow ac windowSize offset |> FSharpFunc<_, _, _>.Adapt *)
+
+    let pw = processWindow intensities width height offset |> FSharpFunc<_, _, _>.Adapt
+
+    //let outputPixels = Array.Parallel.map (fun i -> // These calculations are fixed for the whole array.  Could maybe do some vectorisation of them?
+    let outputPixels = Array.map (fun i -> // These calculations are fixed for the whole array.  Could maybe do some vectorisation of them?
                             let x = i % width
                             let y = i / width
                             pw.Invoke(x, y) |> makeRgb24
