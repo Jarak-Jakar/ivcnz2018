182c591e0fd5fd5a23edf1986e98f32ac8c16442
Jarak-Jakar <jarak_jakar@hotmail.com>
2019-01-18 21:21:19 +1300

    Removed the 'magic numbers' code from Naive
    And changed it's implementation of the computations of the x and y coordinates to using the Math.DivRem function, since that seemed to be the best performer in benchmarking.

--- naive/Program.fs
+++ naive/Program.fs
@@ -14,64 +14,6 @@ open System
 
 let timer = System.Diagnostics.Stopwatch()
 
-(* 
-    Putting in here my mucking around with trying to do the magic number stuff for speeding up the computation of divisors and remainders
-    This is pretty much based on what I read in Hacker's Delight 2nd Edition, specifically in Chapter 10, Figure 10-1
-*)
-
-let magic d = 
-    let ad = abs(d) |> uint32
-    let two31 = 0x80000000u
-    let t = two31 + ((uint32 d) >>> 31)
-    let anc = t - 1u - (t % ad)
-    let mutable p = 31
-    let mutable q1 = two31 / anc
-    let mutable r1 = two31 - q1 * anc
-    let mutable q2 = two31 / ad
-    let mutable r2 = two31 - q2 * ad
-
-    // This is utterly horrific, but it was the best way I could come up with to 
-    // replicate the do-while loop in Hacker's Delight
-    let rec dowhileloop () = 
-        p <- p + 1
-        q1 <- q1 * 2u
-        r1 <- 2u * r1
-
-        if r1 >= anc then
-            q1 <- q1 + 1u
-            r1 <- r1 - anc
-
-        q2 <- 2u * q2
-        r2 <- 2u * r2
-
-        if r2 >= ad then
-            q2 <- q2 + 1u
-            r2 <- r2 - ad
-
-        let delta = ad - r2
-
-        if q1 < delta || q1 = delta && r1 = 0u then
-            dowhileloop()
-        else
-            ()
-    
-
-    let M = if d < 0 then
-                (q2 + 1u) |> int
-            else
-                (q2 + 1u) |> int |> (*) -1
-
-    let s = p - 32
-    (M, s)
-
-let quotrem d m s x = 
-    let q = (x * m) >>> s
-    let t = q * d
-    let r = x - t
-    (q, r)
-
-
-
 let inline findMedian (l: 'a[]) =
     Array.sortInPlace l
     l.[(Array.length l)>>>1]
@@ -102,14 +44,10 @@ let medianFilter (intensities: byte[]) width height windowSize =
 
     let pw = processWindow intensities width height offset |> FSharpFunc<_, _, _>.Adapt
 
-    let m, s = magic width
-    let quotrem' = quotrem width m s
-
-    //let outputPixels = Array.Parallel.map (fun i -> // These calculations are fixed for the whole array.  Could maybe do some vectorisation of them?
-    let outputPixels = Array.map (fun i -> // These calculations are fixed for the whole array.  Could maybe do some vectorisation of them?
-                            (* let x = i % width
-                            let y = i / width *)
-                            let x, y = quotrem' i
+    let outputPixels = Array.Parallel.map (fun i ->
+    //let outputPixels = Array.map (fun i ->
+                            let mutable x = 0
+                            let y = Math.DivRem(i, width, &x)
                             pw.Invoke(x, y) |> findMedian |> makeRgb24
                         ) [|0..intensities.Length-1|]
 
