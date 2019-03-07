ea330f801eefd1b97f7f4a58aafe91dc01c91562
Jarak <jarak_jakar@hotmail.com>
2019-03-08 11:36:17 +1300

    Change cml implementation
    Restructured the whole thing.
    
    Certainly nicer to look at, probably should be faster too.
    
    Currently a bit of an issue in that in order to make the proxels, the proxels need to be made already...
    
    Going to switch that to a two-stage process, so that the proxels are created, and then their alternatives updated to be correct.

--- cml/Program.fs
+++ cml/Program.fs
@@ -11,260 +11,158 @@ open SixLabors.ImageSharp.Advanced
 open SixLabors.Memory
 open SixLabors.ImageSharp.Formats.Png
 
-let inline checkNonNull argName arg =
-            match box arg with
-            | null -> nullArg argName
-            | _ -> ()
+[<Struct>]
+type Exchange<'a when 'a : unmanaged> =
+    | Give
+    | Take of System.ValueTuple<'a, int>
 
-let valuechoose (chooser: 'T -> ValueOption<'U>) (array: 'T[]) =
-            checkNonNull "array" array
-
-            let mutable i = 0
-            let mutable first = Unchecked.defaultof<'U>
-            let mutable found = false
-            while i < array.Length && not found do
-                let element = array.[i]
-                match chooser element with
-                | ValueNone -> i <- i + 1
-                | ValueSome b -> first <- b; found <- true
-
-            if i <> array.Length then
-
-                //let chunk1 : 'U[] = Microsoft.FSharp.Primitives.Basics.Array.zeroCreateUnchecked ((array.Length >>> 2) + 1)
-                let chunk1 : 'U[] = Array.zeroCreate ((array.Length >>> 2) + 1)
-                chunk1.[0] <- first
-                let mutable count = 1
-                i <- i + 1
-                while count < chunk1.Length && i < array.Length do
-                    let element = array.[i]
-                    match chooser element with
-                    | ValueNone -> ()
-                    | ValueSome b -> chunk1.[count] <- b
-                                     count <- count + 1
-                    i <- i + 1
-
-                if i < array.Length then
-                    //let chunk2 : 'U[] = Microsoft.FSharp.Primitives.Basics.Array.zeroCreateUnchecked (array.Length-i)
-                    let chunk2 : 'U[] = Array.zeroCreate (array.Length-i)
-                    count <- 0
-                    while i < array.Length do
-                        let element = array.[i]
-                        match chooser element with
-                        | ValueNone -> ()
-                        | ValueSome b -> chunk2.[count] <- b
-                                         count <- count + 1
-                        i <- i + 1
-
-                    //let res : 'U[] = Microsoft.FSharp.Primitives.Basics.Array.zeroCreateUnchecked (chunk1.Length + count)
-                    let res : 'U[] = Array.zeroCreate (chunk1.Length + count)
-                    System.Array.Copy(chunk1, res, chunk1.Length)
-                    System.Array.Copy(chunk2, 0, res, chunk1.Length, count)
-                    res
-                else
-                    //Microsoft.FSharp.Primitives.Basics.Array.subUnchecked 0 count chunk1
-                    Array.sub chunk1 0 count
-            else
-                Array.empty
-
-// type 'a Pix = {
-//     intensity: 'a
-//     index: int
-//     neighbours: ValueOption<'a> list
-//     chan: Ch<ValueOption<'a>>
-// }
-
-type 'a Pix = {
+//[<Struct>]
+type Proxel<'a when 'a : unmanaged> = { // "proxel" is derived as a combination of picture element (pixel) and processing element
+// Only intensity and neighbourhoodIntensities should really need refreshing after the proxel has been created
+// Currently, the alternatives will also need repopulating, and takesRemaining will need to be reset
     intensity: 'a
     index: int
-    neighbourIndices: int []
-    neighboursIntensities: 'a []
+    neighbourhood: int [] // This keeps a list of the indices in the 1D array of each proxel's neighbour, so that it can simply dive
+                            // into the globals.proxels array at that location to get the necessary neighbour
     chan: Ch<'a>
-    iteration: int
+    neighbourhoodIntensities: 'a []
+    alternatives: Alt<Exchange<'a>> []
+    mutable takesRemaining: uint16
 }
 
-type 'a Globals = {
-    intensities: 'a []
-    height: int
+type Globals<'a when 'a : unmanaged> = {
     width: int
-    windowSize: int
-    offset: int
+    height: int
     pixelCount: int
-    pixels: 'a Pix []
+    intensities: 'a []
+    mutable proxels: Proxel<'a> []
+    windowEdgeSize: int
+    offset: int
+    barrier: Latch
+    finalImageArray: Gray8 []
 }
 
-let determineNeighbourIndices globals index =
-    let mutable x = 0
-    let y = System.Math.DivRem(index, globals.width, &x)
-    let lhb = max 0 (x - globals.offset) // Lower horizontal bound
-    let uhb = min (globals.width - 1) (x + globals.offset) // Upper horizontal bound
-    let lvb = max 0 (y - globals.offset) // Lower vertical bound
-    let uvb = min (globals.height - 1) (y + globals.offset) // Upper vertical bound
-    let mutable arrayIdx = 0
-    let arraySize = min ((uvb - lvb) * (uhb - lhb) - 1) (globals.windowSize * globals.windowSize - 1)
-    let indices = Array.zeroCreate arraySize
-
-    for w in lvb..uvb do
-        for z in lhb..uhb do
-            if w = z then ()
-            else
-                indices.[arrayIdx] <- z + globals.width * w
-                arrayIdx <- arrayIdx + 1
-
-    indices
-
-
-let createPixelProcess globals index =
-    //let intens = globals.intensities.[index]
-    let neighbourIndices = determineNeighbourIndices globals index
-    {
-        intensity = globals.intensities.[index]
-        index = index
-        neighbourIndices = neighbourIndices
-        neighboursIntensities = Array.zeroCreate (Array.length neighbourIndices)
-        chan = Ch ()
-        iteration = 0
-    }
-
-[<Struct>]
-type 'a Choice =
-    | Give
-    | Take of ('a ValueOption * int)
-
-// let listDisplacements ws =
-//     let ub = (ws - 1) / 2
-//     let lb = -ub
-//     let mutable disps = List.empty
-//     for i in lb..ub do
-//         for j in lb..ub do
-//             if i = 0 && j = 0 then
-//                 ()
-//             else
-//                 disps <- (i, j) :: disps
-//     disps
-
-// let arrayDisplacements ws =
-//     let ub = (ws - 1) / 2
-//     let lb = -ub
-//     let disps = Array.zeroCreate (ws - 1)
-//     let mutable idx = 0
-//     for i in lb..ub do
-//         for j in lb..ub do
-//             if i = 0 && j = 0 then
-//                 ()
-//             else
-//                 disps.[idx] <- (i, j)
-//                 idx <- idx + 1
-//     disps
-
-
-let findIndex width x y =
-    x + width * y
-
-let findCoords width index =
-    (index % width, index / width)
-
-let makeNeighboursIndexList' pix coordFinder indexFinder windowSize =
-    let x,y = coordFinder pix.index
-    listDisplacements windowSize |>
-    List.map (fun (dx, dy) ->
-        indexFinder (x + dx) (y + dy)
-    )
-
-let makeNeighboursIndexArray' pix coordFinder indexFinder windowSize =
-    let x,y = coordFinder pix.index
-    arrayDisplacements windowSize |>
-    Array.map (fun (dx, dy) ->
-        indexFinder (x + dx) (y + dy)
-    )
-
-// let takeIntensity pixels neighbourIndex =
-//     if neighbourIndex < 0 || neighbourIndex >= (Array.length pixels) then
-//         Alt.always (Take(ValueNone, neighbourIndex))
-//     else
-//         Ch.take pixels.[neighbourIndex].chan
-//             ^-> (fun i -> Take (i, neighbourIndex))
-
-let takeIntensity (channels: 'a Ch []) neighbourIndex =
-    Ch.take channels.[neighbourIndex] ^-> (fun i -> Take(i, neighbourIndex))
-
-
-let inline findListMedian l =
-    List.sort l
-    |> (fun m -> m.[m.Length / 2])
+//let makeRgb24 intensity = Rgb24(intensity, intensity, intensity)
+let inline makeGray8 intensity = Gray8(intensity)
 
 let inline findArrayMedian l =
     Array.sortInPlace l
     l.[(Array.length l) >>> 1]
 
+let give proxel =
+    Ch.give proxel.chan proxel.intensity ^->. Give
 
-let buildAlts' globals pix neighbour =
-    let give = pix.chan *<- pix.intensity
-                ^->. Give
-    let take = takeIntensity globals.pixels neighbour
-    //[give; take]
-    give <|> take
+let take globals proxel neighbourIndex =
+    let neighbour = globals.proxels.[proxel.neighbourhood.[neighbourIndex]]
+    Ch.take neighbour.chan ^-> fun received -> Take (System.ValueTuple.Create(received, neighbourIndex))
 
-let makeRgb24 intensity = Rgb24(intensity, intensity, intensity)
+let determineNeighbours globals index =
+    let mutable x = 0
+    let y = System.Math.DivRem(index, globals.width, &x)
 
-let runPixel coordFinder indexFinder globals barrier (outputArray: Rgb24 []) pix =
-    let neighboursIndexList = makeNeighboursIndexList' pix coordFinder indexFinder globals.windowSize
-    let ba = buildAlts' globals pix
-    let alts = ba (List.head neighboursIndexList)
-    job {
-        do! Job.iterateServer ((List.tail neighboursIndexList), pix, alts) <| fun (neighbours, p, alts) ->
-                Alt.choose alts |> Alt.afterFun (fun x ->
-                                                    match x with
-                                                    | Give -> (neighbours, p, alts)
-                                                    | Take(n,i) ->
-                                                        if List.isEmpty neighbours then
-                                                            //let median = List.choose id p.neighbours |> findListMedian
-                                                            let median = valuechoose id p.neighbours |> findArrayMedian
-                                                            outputArray.[p.index] <- median |> makeRgb24
-                                                            Latch.decrement barrier |> run
-                                                            ([], p, [pix.chan *<- pix.intensity ^->. Give])
-                                                        else
-                                                            let newAlts = ba (List.head neighbours)
-                                                            ((List.tail neighbours), {p with neighbours = n :: p.neighbours}, newAlts)
+    let lhb = max 0 (x - globals.offset) // Lower horizontal bound
+    let uhb = min (globals.width - 1) (x + globals.offset) // Upper horizontal bound
+    let lvb = max 0 (y - globals.offset) // Lower vertical bound
+    let uvb = min (globals.height - 1) (y + globals.offset) // Upper vertical bound
 
-                )
-        return pix
+    let neighbours = Array.zeroCreate ((uhb - lhb + 1) * (uvb - lvb + 1))
+    let mutable idx = 0
+
+    for w in lvb..uvb do
+        for z in lhb..uhb do
+            neighbours.[idx] <- z + globals.width * w
+            idx <- idx + 1
+
+    neighbours
+
+
+let makeProxel globals index =
+    let neighbourhood = determineNeighbours globals index
+    let proxelsIntensity = globals.intensities.[index]
+    let proxel = {
+        intensity = proxelsIntensity
+        index = index
+        neighbourhood = neighbourhood
+        chan = Ch ()
+        neighbourhoodIntensities = Array.zeroCreate (Array.length neighbourhood)
+        alternatives = null
+        takesRemaining = Array.length neighbourhood |> uint16
     }
+    proxel.neighbourhoodIntensities.[((Array.length neighbourhood) - 1)] <- proxelsIntensity
+    let takes = Seq.init (Array.length neighbourhood) (take globals proxel)
+    let alternatives = Seq.append takes (Seq.singleton (give proxel)) |> Seq.toArray
+    // Currently the implementation relies on the give being at the end of the array
+    // As this way the neighbourIndex returned from a take corresponds directly to the same position in the alternatives list
+    {proxel with alternatives = alternatives}
 
-let storeMedians (arr: Rgb24 []) oachan = job {
-    let! (index, median) = Ch.take oachan
-    arr.[index] <- makeRgb24 median
-}
+let processExchange prox = function
+    | Give -> prox
+    | Take(struct (intensity, index)) ->
+            prox.neighbourhoodIntensities.[index] <- intensity
+            prox.alternatives.[index] <- Alt.never()
+            prox.takesRemaining <- prox.takesRemaining - 1us
+            prox
+
+let inline storeValueAtEnd globals proxel =
+    let finalValue = findArrayMedian proxel.neighbourhoodIntensities |> makeGray8
+    globals.finalImageArray.[proxel.index] <- finalValue
+
+let checkFinishedAndSetOutput globals proxel =
+    if proxel.takesRemaining = 0us then
+        // output finalValue
+        storeValueAtEnd globals proxel
+        // decrement barrier
+        globals.barrier.Decrement() |> run
+
+    proxel
+
+let runProxel globals proxel =
+    Job.iterateServer proxel (fun prox ->
+                                    Alt.choosy proxel.alternatives |>
+                                    Alt.afterFun (processExchange prox) |>
+                                    Alt.afterFun (checkFinishedAndSetOutput globals)
+    )
+
+let inline makeImage globals =
+    Image.LoadPixelData(globals.finalImageArray, globals.width, globals.height)
 
 let medianFilter globals =
-    //let pixelCount = globals.width * globals.height
-    let fc = findCoords globals.width
-    let fi = findIndex globals.width
-    let barrier = Hopac.Latch globals.pixelCount
-    let outputArray = Array.zeroCreate globals.pixelCount
-    let pixelCreator = createPixelProcess globals
-    let pixels = Array.Parallel.init globals.pixelCount pixelCreator
-    let newGlobals = {globals with pixels = pixels}
-    //let pixels = Array.Parallel.mapi (fun i x -> {intensity = x; index = i; neighbours = [ValueSome(x)]; chan = Ch ();}) globals.intensities
-    let runpix = runPixel fc fi newGlobals barrier outputArray
+    let proxelMaker = makeProxel globals
 
-    Seq.Con.mapJob runpix pixels |> run |> ignore
-    job {do! (Latch.await barrier)} |> run
-    Image.LoadPixelData(outputArray, globals.width, globals.height)
+    let proxels = Array.Parallel.init globals.pixelCount proxelMaker
+    globals.proxels <- proxels
+
+    Hopac.Extensions.Array.iterJob (runProxel globals) proxels |> run
+    job {do! (Latch.await globals.barrier)} |> run
+
+    makeImage globals
 
 [<EntryPoint>]
 let main argv =
     let filename = argv.[0]
     let numIterations = int argv.[1]
-    let windowSize = int argv.[2]
-    //let offset = (windowSize / 2) - 1
+    let windowEdgeSize = int argv.[2]
+    let offset = (windowEdgeSize - 1) >>> 1
 
     Configuration.Default.MemoryAllocator <- ArrayPoolMemoryAllocator.CreateWithModeratePooling()
 
-    use img: Image<Rgb24> = Image.Load(@"..\..\Images\Inputs\" + filename)
+    use img = Image.Load(@"..\..\Images\Inputs\" + filename)
 
     img.Mutate(fun x -> x.Grayscale() |> ignore)
 
-    let mutable outImg = new Image<Rgb24>(img.Width, img.Height)
+    let globals = {
+        intensities = null
+        width = img.Width
+        height = img.Height
+        pixelCount = img.Width * img.Height
+        proxels = null
+        windowEdgeSize = windowEdgeSize
+        offset = offset
+        barrier = Hopac.Latch(img.Width * img.Height)
+        finalImageArray = Array.zeroCreate (img.Width * img.Height)
+    }
+
+    let mutable outImg = new Image<Gray8>(img.Width, img.Height)
 
     let timer = System.Diagnostics.Stopwatch ()
 
@@ -274,28 +172,16 @@ let main argv =
 
         timer.Start ()
 
-        //let imageWidth = img.Width
-        //let imageHeight = img.Height
-        //let pixelCount = imageWidth * imageHeight
-        let intensities = img.GetPixelSpan().ToArray() |> Array.Parallel.map (fun p -> p.R)
-        //outImg <- medianFilter intensities img.Width img.Height windowSize
-        let globals = {
-            intensities = intensities
-            width = img.Width
-            height = img.Height
-            pixelCount = img.Width * img.Height
-            windowSize = windowSize
-            offset = (windowSize / 2) - 1
-            pixels = null
-        }
-        outImg <- medianFilter globals
+        let imageIntensities = img.GetPixelSpan().ToArray() |> Array.Parallel.map (fun p -> p.R)
+
+        outImg <- medianFilter {globals with intensities = imageIntensities; barrier = Hopac.Latch(globals.pixelCount)}
         timer.Stop ()
 
     use outFile = new System.IO.FileStream(@"..\..\Images\Outputs\cml_" + System.IO.Path.GetFileNameWithoutExtension(filename) +
-                    "_" + string windowSize +  ".png", System.IO.FileMode.OpenOrCreate)
+                    "_" + string windowEdgeSize +  ".png", System.IO.FileMode.OpenOrCreate)
 
     let pngenc = PngEncoder()
-    pngenc.ColorType <- PngColorType.Rgb
+    pngenc.ColorType <- System.Nullable(PngColorType.Grayscale)
     outImg.Save(outFile, pngenc)
 
     let totalTimeTaken = timer.Elapsed.TotalSeconds
